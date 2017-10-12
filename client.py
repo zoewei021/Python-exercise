67221ed26be932223380d56993c0502861b517f0
unknown <weishengqin@tclking.com>
2017-10-12 09:42:40 +0800

    add 2 files

---
+++ client.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+# author：zoe
+# client 客户端
+import socket
+
+client = socket.socket()
+client.connect(('localhost', 9897))
+
+while True:
+    msg = input(">>:").strip()
+    if len(msg) == 0: continue
+    client.send(msg.encode("utf-8"))#按utf-8的方式编码，转成bytes
+    data = client.recv(10240000)
+    f=open("")
+    print("recv:", data.decode())
+
+client.close()
